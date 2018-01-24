e34281bd0f4c7bafa47863c39009c7a990484216
skaefer143 <stormkaefer@gmail.com>
2018-01-24 13:25:38 -0700

    Add about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,18 @@ export default class About extends React.Component {
   
   render() {
     return (
-      <div></div>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2>About</h2>
+            </div>
+            <div className="col-lg-12 text-center">
+              <p>You can find more information here: https://github.com/skaefer143/MTM-UI</p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
   
